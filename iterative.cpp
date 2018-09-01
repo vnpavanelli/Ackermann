4a0e7cc7e451f4ab127cdf644abc6988a9217f2f
Vinicius Pavanelli <subkelvin@gmail.com>
2018-08-31 21:29:05 -0300

    Adicionada iterativa2 e checagem da resposta

--- iterative.cpp
+++ iterative.cpp
@@ -30,4 +30,35 @@ unsigned int ackermann_iterative(unsigned int m, unsigned int n) {
 	return 0;
 }
 
+unsigned int ackermann_iterative2(unsigned int m, unsigned int n) {
+	std::stack<unsigned int> pilha;
+//	pilha.push(m);
+//	pilha.push(n);
+	while (1) {
+//		n = pilha.top();
+//		pilha.pop();
+//		if (pilha.empty()) {
+//			return n;
+//		}
+//		m = pilha.top();
+//		pilha.pop();
+		if (m==0) {
+			n++;
+			if (pilha.empty()) return n;
+			m = pilha.top();
+			pilha.pop();
+			continue;
+		} else if (n==0) {
+			m--;
+			n = 1;
+			continue;
+		} else {
+			pilha.push(m-1);
+			n--;
+			continue;
+		}
+	}
+	return 0;
+}
+
 
--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include <string>
 
 const bool DEBUG = false;
-const unsigned int LIMIT_M = 4;
-const unsigned int LIMIT_N = 14;
+const unsigned int LIMIT_M = 2;
+const unsigned int LIMIT_N = 10;
 typedef unsigned int (*f_ptr)(unsigned int, unsigned int);
 
 struct _f {
@@ -14,21 +14,28 @@ struct _f {
 	std::string nome;
 };
 
-typedef std::chrono::duration<double> tempo;
+struct _r {
+	std::chrono::duration<double> seconds;
+	unsigned int value;
+};
 
 unsigned int ackermann_recursive(unsigned int, unsigned int);
 unsigned int ackermann_iterative(unsigned int, unsigned int);
+unsigned int ackermann_iterative2(unsigned int, unsigned int);
 extern "C" unsigned int ackermann_iterative_asm(unsigned int, unsigned int);
 extern "C" unsigned int ackermann_iterative_asm2(unsigned int, unsigned int);
 
-tempo bench( const _f &f, unsigned int m, unsigned int n) {
+_r bench( const _f &f, unsigned int m, unsigned int n) {
 	auto start = std::chrono::system_clock::now();
 	auto resposta = f.funcao(m,n);
 	auto end = std::chrono::system_clock::now();
 //	std::cout << f.nome << "(" << m << "," << n << ") = " << resposta << '\t' << std::flush;
 	std::chrono::duration<double> elapsed_seconds = end-start;
 //	std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
-	return elapsed_seconds;
+	_r r;
+	r.seconds = elapsed_seconds;
+	r.value = resposta;
+	return r;
 }
 
 int main (int ac, char **av) {
@@ -36,19 +43,31 @@ int main (int ac, char **av) {
 	std::vector<_f> funcoes = {
 		{ ackermann_recursive, "recursiva" },
 		{ ackermann_iterative, "iterativa" },
+		{ ackermann_iterative2, "iterativa2" },
 		{ ackermann_iterative_asm, "asm1" },
 		{ ackermann_iterative_asm2, "asm2" }
 	};
 	std::cout << std::endl << "(m,n)\t";
 	for (auto &i : funcoes) std::cout << i.nome << "\t";
-	std::cout << std::endl;
+	std::cout << "\tValue" << std::endl;
 	for (int m = 0; m < LIMIT_M; m++) {
 		for (int n = 0; n < LIMIT_N; n++) {
 			std::cout << "(" << m << "," << n << ")" << std::flush;
+			bool first = true;
+			unsigned int value=0;
 			for (auto &i : funcoes) {
-				std::cout << "\t" << bench(i, m, n).count() << std::flush;
+				_r tmp = bench(i, m, n);
+				std::cout << "\t" << tmp.seconds.count() << std::flush;
+				if (first) {
+					value = tmp.value;
+					first = false;
+				} else {
+					if (value != tmp.value) {
+						std::cout << "X" << std::flush;
+					}
+				}
 			}
-			std::cout << std::endl;
+			std::cout << "\t" << value << std::endl;
 		}
 	}
 
